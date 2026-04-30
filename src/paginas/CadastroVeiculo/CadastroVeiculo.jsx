c37a8a396ac5649c3596d9e12a428d3dba4092c3
Everton Melo <tetoferreira04@gmail.com>
2026-04-30 12:34:00 -0300

    Salvando1/3

--- src/paginas/CadastroVeiculo/CadastroVeiculo.jsx
+++ src/paginas/CadastroVeiculo/CadastroVeiculo.jsx
@@ -31,10 +31,40 @@ const listagem = veiculo.id?  "Editar Veiculo" : "Novo Veiculo";
     <Principal>
       <CampoCustomizado
         label="Marca"
-        obrigatorio
+          opcoes={[
+          {valor:"1" , label: "Fiat"},
+          {valor:"2" , label: " Audi"},
+          {valor:"3" , label: " BMW"},
+          {valor:"4" , label: "BYD"},
+          {valor:"5" , label: "CAOA Chery"},
+          {valor:"6" , label: "Chevrolet"},
+          {valor:"7" , label: "Citroen"},
+          {valor:"8" , label: "Ford"},
+          {valor:"9" , label: "GWM"},
+          {valor:"10" , label: "Honda"},
+          {valor:"11" , label: "Hyundai"},
+          {valor:"12" , label: "Jac Motors"},
+          {valor:"13" , label: "Jeep"},
+          {valor:"14" , label: "Kia"},
+          {valor:"15" , label: "Lifan"},
+          {valor:"16" , label: "Mercedes-Benz"},
+          {valor:"17" , label: "Mitsubishi"},
+          {valor:"18" , label: "Nissan"},
+          {valor:"19" , label: "Peugeot"},
+          {valor:"20" , label: "Renault"},
+          {valor:"21" , label: "Subaru"},
+          {valor:"22" , label: "Suzuki"},
+          {valor:"23" , label: "Toyota"},
+          {valor:"24" , label: "Volkswagen"},
+          {valor:"25" , label: "Volvo"},
+          {valor:"26" , label: "Land Rover"},
+          {valor:"27" , label: "MINI"},
+          {valor:"28" , label: "RAM"},
+        ]}
         value={veiculo.marca}
         onChange={(e) => setVeiculo({ ...veiculo, marca: e.target.value })}
-      /> 
+        obrigatorio
+      />
 
       <CampoCustomizado
         label="Modelo"
@@ -61,6 +91,10 @@ const listagem = veiculo.id?  "Editar Veiculo" : "Novo Veiculo";
         value={veiculo.placa}
         onChange={(e) => setVeiculo({ ...veiculo, placa: e.target.value })}
       />
+
+      <Botao className="botao-confirmar" aoClicar={salvar}>
+        Confirmar
+      </Botao>
 
       </Principal>
   )
